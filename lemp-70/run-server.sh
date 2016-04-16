279d192c47eb1e9510e36c2d5d62ac08ede95e37
Borales <bordun.alexandr@gmail.com>
2016-04-16 12:08:10 +0200

    Starting cron

--- lemp-70/run-server.sh
+++ lemp-70/run-server.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-sudo service ssh start
-sudo service rsyslog start
-sudo service nginx start
-sudo service php7.0-fpm start
+service ssh start
+service rsyslog start
+service nginx start
+service php7.0-fpm start
+cron
 
 tail -f /dev/null
